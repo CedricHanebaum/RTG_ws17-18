a7ceb772feef075045f69f61dcb91d5a8efc46b5
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2017-11-29 12:50:34 +0100

    typo fix in A4

--- assignment04/Cloth.cc
+++ assignment04/Cloth.cc
@@ -172,7 +172,7 @@ void Cloth::updateForces()
     // Damping
     for (auto& p : particles)
     {
-        /// Task 2.d
+        /// Task 2.c
         /// For the system to come to rest, damping is applied.
         ///
         /// your job is to:
@@ -204,7 +204,7 @@ void Cloth::updateMotion(float elapsedSeconds)
         ///     - update the particle position (p.position)
         ///
         /// Notes:
-        ///     - particle force is stored in p.accumulatedForce
+        ///     - particle force is stored in p.accumulatedForces
         ///     - particle mass is stored in p.mass
         ///
         /// ============= STUDENT CODE BEGIN =============
