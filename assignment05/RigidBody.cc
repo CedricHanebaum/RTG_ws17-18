2182076d88bea82f2cd7a8f0673afdb2c7750f98
Cedric Hanebaum <Cedric.Hanebaum@tsaro.io>
2017-12-05 19:52:30 +0100

    Implemented Task 2.a

--- assignment05/RigidBody.cc
+++ assignment05/RigidBody.cc
@@ -36,7 +36,19 @@ void RigidBody::calculateMassAndInertia()
 	{
 		// s.position (sample position in body space)
 		// s.mass     (sample mass)
+        mass += s.mass;
+        cog += s.mass * s.position; // maybe convert position to world space here?
 	}
+
+    cog *= (1 / mass);
+
+    for(auto const& s: samples) {
+        auto r = s.position - cog;
+        // column major order
+        inertia += s.mass * glm::mat3(glm::vec3(std::pow(r.y, 2) + std::pow(r.z, 2), -r.y * r.x, -r.z * r.x),
+                                      glm::vec3(-r.x * r.y, std::pow(r.z, 2) + std::pow(r.x, 2), -r.z * r.y),
+                                      glm::vec3(-r.x * r.z, -r.y * r.z, std::pow(r.x, 2) + std::pow(r.y, 2)));
+    }
 
 /// ============= STUDENT CODE END =============
 
